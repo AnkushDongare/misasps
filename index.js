684c8a9fff161ed3fec0f611ad39cb3e2f443dac
AnkushDongare <dongreankush52@gmail.com>
2024-06-10 19:52:28 +0530

    Some change in response id

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ app.post('/submit', async (req, res) => {
             response: responses
         });
         const savedResponse = await newResponse.save();
-        res.status(200).send({"message":'Responses submitted successfully!', });
+        res.status(200).send({message:'Responses submitted successfully!', responseId: newResponse._id});
     } catch (error) {
         console.error('Error submitting responses:', error);
         res.status(500).json({ error: 'Server error' });
